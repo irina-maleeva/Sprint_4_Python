11dc63663cded10dc27e54ccb8ef520d5371b2bd
irina-maleeva <iwmaleeva@gmail.com>
2023-03-23 12:30:51 +0300

    new content

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture()
+def driver():
+    driver = webdriver.Chrome()
+    driver.get('https://qa-scooter.praktikum-services.ru/')
+    yield driver
+    driver.quit()
